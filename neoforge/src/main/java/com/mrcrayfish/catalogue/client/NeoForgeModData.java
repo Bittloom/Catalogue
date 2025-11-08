00fbc8027c99d50b6f5513272e5460b67065d04e
Bittloom <armenta.salomon@icloud.com>
2025-11-08 01:30:44 +0000

    Added new string conditions

--- neoforge/src/main/java/com/mrcrayfish/catalogue/client/NeoForgeModData.java
+++ neoforge/src/main/java/com/mrcrayfish/catalogue/client/NeoForgeModData.java
@@ -204,7 +204,7 @@ public class NeoForgeModData implements IModData
     {
         // For Fabric libraries loaded by Sinytra Connector
         String modId = info.getModId();
-        if(modId.startsWith("fabric_") || modId.equals("fabricloader") || modId.equals("mixinextras"))
+        if(modId.startsWith("fabric_") || modId.contains("lib") || modId.equals("fabricloader") || modId.equals("mixinextras")) || modId.equals("connector") || modId.equals("kotlinforforge") || modId.equals("blueprint")
         {
             return Type.LIBRARY;
         }
